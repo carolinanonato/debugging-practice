9a91105c086adc7ce283b27eb2839d8118db1bcc
Carolina Nonato <csnonato@gmail.comgit config --global user.email>
2021-11-22 20:49:22 -0500

    player can now choose colours for the back card

--- exercise/scripts/main.js
+++ exercise/scripts/main.js
@@ -41,7 +41,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
       }
       // If we've matched all the cards, display a message.
-      if (matchedCards.length === cards.length) {
+      if (matchedCards.length == cards.length) {
         alert('You matched all the cards, nice job!');
       } 
     });
@@ -52,8 +52,8 @@ window.addEventListener('DOMContentLoaded', function() {
   // - #green
   // - #orange
   // - #red
-  var deckElement = document.querySelector('.Cards');
-  var deckColor = window.location.hashh;
+  var deckElement = document.querySelector('.cards');
+  var deckColor = window.location.hash.substr(1);
 
   if (deckElement && deckColor) {
     var className = 'cards--' + deckColor;
